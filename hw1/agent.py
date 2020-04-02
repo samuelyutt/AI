077f5d4b7ac5ed41f8439d852f284c9857d15b60
Samuel Yu <samuelyuyy@gmail.com>
2020-04-02 23:11:25 +0800

    Add IDAStar

--- hw1/agent.py
+++ hw1/agent.py
@@ -145,6 +145,43 @@ class astar(agent):
         return []
 
 
+class idastar(agent):
+    def __init__(self, start_, goal_):
+        super(idastar, self).__init__(start_, goal_)
+
+    def search(self, b):
+        depth_limit = -1
+
+        while True:
+            depth_limit += 1
+
+            root = node(None, self.start, 0)
+            self.node_count += 1
+
+            explorered_set = []
+            frontier = []
+            pair = (estimated_cost(root.depth, root, self.goal), root)
+            bisect.insort(frontier, pair)
+            
+            while len(frontier):            
+                cur_node = frontier.pop(0)[1]
+                if cur_node.depth == depth_limit:
+                    continue
+                possible_moves = cur_node.position.available_moves(b)
+
+                for new_pos in possible_moves:
+                    if new_pos not in explorered_set:
+                        child = node(cur_node, new_pos, cur_node.depth+1)
+                        cur_node.add_child(child)
+                        pair = (estimated_cost(child.depth, child, self.goal), child)
+                        bisect.insort(frontier, pair)
+                        self.node_count += 1
+                        explorered_set.append(new_pos)
+                        if new_pos == self.goal:
+                            return path_list(child)
+        return []
+
+
 if __name__ == '__main__':
     b = board(8)
     p_start = position(0, 0)
@@ -156,16 +193,21 @@ if __name__ == '__main__':
     print(a.node_count)
 
     a = dfs(p_start, p_goal)
-    path= a.search(b)
+    path = a.search(b)
     b.print_pathway(path)
     print(a.node_count)
 
     a = ids(p_start, p_goal)
-    path= a.search(b)
+    path = a.search(b)
     b.print_pathway(path)
     print(a.node_count)
 
     a = astar(p_start, p_goal)
-    path= a.search(b)
+    path = a.search(b)
+    b.print_pathway(path)
+    print(a.node_count)
+
+    a = idastar(p_start, p_goal)
+    path = a.search(b)
     b.print_pathway(path)
     print(a.node_count)
