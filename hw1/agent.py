7295c63034005053e3102048f160cbfcd3ad9e9f
Samuel Yu <samuelyuyy@gmail.com>
2020-04-02 22:54:30 +0800

    Add AStar

--- hw1/agent.py
+++ hw1/agent.py
@@ -1,7 +1,13 @@
 import queue
+import bisect
 from board import board
 from board import position
 from tree import node
+
+def estimated_cost(g, node, goal):
+    pos = node.position
+    heuristic = int( (abs(pos.x-goal.x)+abs(pos.y-goal.y)) / 3 )
+    return g + heuristic
 
 def path_list(goal_node):
     ret = []
@@ -84,7 +90,7 @@ class ids(agent):
 
         while True:
             depth_limit += 1
-            
+
             root = node(None, self.start, 0)
             self.node_count += 1
 
@@ -109,6 +115,36 @@ class ids(agent):
         return []
 
 
+class astar(agent):
+    def __init__(self, start_, goal_):
+        super(astar, self).__init__(start_, goal_)
+
+    def search(self, b):
+        root = node(None, self.start, 0)
+        self.node_count += 1
+
+        explorered_set = []
+        frontier = []
+        pair = (estimated_cost(root.depth, root, self.goal), root)
+        bisect.insort(frontier, pair)
+        
+        while len(frontier):            
+            cur_node = frontier.pop(0)[1]
+            possible_moves = cur_node.position.available_moves(b)
+
+            for new_pos in possible_moves:
+                if new_pos not in explorered_set:
+                    child = node(cur_node, new_pos, cur_node.depth+1)
+                    cur_node.add_child(child)
+                    pair = (estimated_cost(child.depth, child, self.goal), child)
+                    bisect.insort(frontier, pair)
+                    self.node_count += 1
+                    explorered_set.append(new_pos)
+                    if new_pos == self.goal:
+                        return path_list(child)
+        return []
+
+
 if __name__ == '__main__':
     b = board(8)
     p_start = position(0, 0)
@@ -125,6 +161,11 @@ if __name__ == '__main__':
     print(a.node_count)
 
     a = ids(p_start, p_goal)
+    path= a.search(b)
+    b.print_pathway(path)
+    print(a.node_count)
+
+    a = astar(p_start, p_goal)
     path= a.search(b)
     b.print_pathway(path)
     print(a.node_count)
--- hw1/tree.py
+++ hw1/tree.py
@@ -2,17 +2,22 @@ from board import position
 
 
 class node:
-	def __init__(self, parent_, position_, depth_ = -1):
-		self.parent = parent_
-		self.position = position_
-		self.childs = []
-		self.depth = depth_
+    def __init__(self, parent_, position_, depth_ = -1):
+        self.parent = parent_
+        self.position = position_
+        self.childs = []
+        self.depth = depth_
 
-	def __str__(self):
-		return self.position.__str__()
+    def __str__(self):
+        return self.position.__str__()
 
-	def __repr__(self):
-		return self.position.__str__()
+    def __repr__(self):
+        return self.position.__str__()
 
-	def add_child(self, child_node):
-		self.childs.append(child_node)
+    def __lt__(self, other):
+        # Used for bisect.insort()
+        # Always return the first object since order of node is unnecessary
+        return self
+
+    def add_child(self, child_node):
+        self.childs.append(child_node)
